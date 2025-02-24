e5e2b239decbba3871ef48c87b6fd1c16d601efe
ashna <ashnacp0204@gmail.com>
2025-02-24 16:12:49 +0530

    added counter file

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter,setcounter] =useState(15)
+  // let counter =15
+  const addvalue = () =>{
+    setcounter((prevcounter)=> prevcounter + 1 )
+    setcounter((prevcounter) => prevcounter + 1);
+    setcounter((prevcounter) => prevcounter + 1);
+    setcounter((prevcounter) => prevcounter + 1);
+  // setcounter(counter+1)
+    console.log(counter);
+    
+  }
+  const removevalue = () =>{
+    setcounter(counter - 1)
+    console.log(counter);
+    
+  }
+
+  return (
+    <>
+    <h1>React course {counter}</h1>
+    <h2>Counter: {counter}</h2>
+    <button onClick ={addvalue}>add value {counter}</button>
+    <button onClick={removevalue}>remove value {counter}</button>{""}
+    <footer>footer: {counter}</footer>
+   </>
+  )
+}
+
+export default App
