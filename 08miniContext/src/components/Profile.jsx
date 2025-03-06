1a219b7c729ce3724034908805be962661faaed6
ashna <ashnacp0204@gmail.com>
2025-03-06 06:54:59 +0530

    Context Api

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useContext } from 'react'
+import UserContext from '../context/userContext'
+
+function Profile() {
+  const {user} = useContext(UserContext)
+  if (!user) return <h1>Not logged in </h1>;
+  return (
+    <h1>Profile: {user.username}</h1>
+  )
+}
+
+export default Profile
